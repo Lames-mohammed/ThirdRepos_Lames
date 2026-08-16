5fca0be9a3e36f6c9282ab20c53e02ce143f751f
001247568 <lames.mohammed@student.tafesa.edu.au>
2026-08-16 19:10:12 +0930

    commit the back stall ticket changes

--- MyInitial/TicketsForm.cs
+++ MyInitial/TicketsForm.cs
@@ -39,11 +39,23 @@ namespace Ticketing
                 { mSection = 2; }
             if (radBox.Checked)
                 { mSection = 3; }
+            if(radioButton1.Checked)
+            { mSection = 4; }
 
             mTicketPrice = new TicketPrice(mSection, mQuantity, mDiscount);
 
             mTicketPrice.calculatePrice();
             lblAmount.Text = System.Convert.ToString(mTicketPrice.AmountDue);
         }
-     }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radGeneral_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
